a31e89960ae1ab94ada742499797d780317b8b59
Bastien <bastien.ratat@gmail.com>
2020-09-25 12:19:03 +0200

    a request can't have more than 5 volunteers and 5 chats at a time

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,10 +1,10 @@
 class ChatsController < ApplicationController
   before_action :set_chat, only: [:show, :update, :destroy]
-  before_action :set_id, only: [:index]
+  before_action :set_id, only: [:index, :create]
 
   # GET /chats
   def index
-    @chats = Chat.all.where(request_id: @req_id)
+    @chats = Chat.all.where(request_id: @request_id)
     render json: @chats
   end
 
@@ -16,11 +16,18 @@ class ChatsController < ApplicationController
   # POST /chats
   def create
     @chat = Chat.new(chat_params)
-
-    if @chat.save
-      render json: @chat, status: :created
+    chats = Chat.all.where(request_id: @request_id)
+    if chats.length() >= 5
+      render json: {
+        status: :forbidden,
+        'error': 'This request cannot have more chatrooms'
+      }
     else
-      render json: @chat.errors, status: :unprocessable_entity
+      if @chat.save
+        render json: @chat, status: :created
+      else
+        render json: @chat.errors, status: :unprocessable_entity
+      end
     end
   end
 
@@ -46,8 +53,9 @@ class ChatsController < ApplicationController
 
     # Allows to retrieve multiple chats for a single request using a request_id param
     def set_id
-      @req_id = Chat.find(params[:request_id])
+      @request_id = Chat.find(params[:request_id])
     end
+
     # Only allow a trusted parameter "white list" through.
     def chat_params
       params.permit(:user_id, :request_id, :volunteer_id)
--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,12 +1,11 @@
 class VolunteersController < ApplicationController
   before_action :set_volunteer, only: [:show, :update, :destroy]
-  before_action :set_id, only: [:index]
+  before_action :set_id, only: [:index, :create]
 
   # GET /volunteers
   def index
     # INDEX ACTION HAS TO RETRIEVE ALL THE VOLUNTEERS FOR A SPECIFIC REQUEST
-    @volunteers = Volunteer.all.where(request_id: @req_id)
-
+    @volunteers = Volunteer.all.where(request_id: @request_id)
     render json: @volunteers
   end
 
@@ -18,11 +17,18 @@ class VolunteersController < ApplicationController
   # POST /volunteers
   def create
     @volunteer = Volunteer.new(volunteer_params)
-
-    if @volunteer.save
-      render json: @volunteer, status: :created
+    volunteers = Volunteer.all.where(request_id: @request_id)
+    if volunteers.length() >= 5
+      render json: {
+        status: :forbidden,
+        'error': 'This request cannot have more volunteers'
+      }
     else
-      render json: @volunteer.errors, status: :unprocessable_entity
+      if @volunteer.save
+        render json: @volunteer, status: :created
+      else
+        render json: @volunteer.errors, status: :unprocessable_entity
+      end
     end
   end
 
@@ -48,7 +54,7 @@ class VolunteersController < ApplicationController
 
     # REQUEST_ID GIVES AN ID TO USE TO CALL THE INDEX ACTION
     def set_id
-      @req_id = Volunteer.find(params[:request_id])
+      @request_id = Volunteer.find(params[:request_id])
     end
 
     # Only allow a trusted parameter "white list" through.
