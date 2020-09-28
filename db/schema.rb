64ce11827654c3bdebb1f35b29044d6371498f27
Bastien <bastien.ratat@gmail.com>
2020-09-28 11:35:21 +0200

    add a approved column to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_110357) do
+ActiveRecord::Schema.define(version: 2020_09_28_092957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 2020_09_25_110357) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.boolean "approved", default: false
   end
 
   create_table "volunteers", force: :cascade do |t|
